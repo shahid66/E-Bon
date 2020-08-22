837898a0fd7075598fd58c26003ff350a3bfe627
SBN <amdadulislamcl@gmail.com>
2020-08-23 01:16:02 +0600

    route structure added

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('users/home');
-});
-Route::get('/a', function () {
-    return view('users/checkOut');
-});
-Route::get('/b', function () {
-    return view('users/profile');
-});
-Route::get('/admin', function () {
-    return view('admin/index');
-});
-Route::get('/product', function () {
-    return view('admin/product/index');
-});
-Route::get('/product/create', function () {
-    return view('admin/product/create');
-});
-Route::get('/product/show', function () {
-    return view('admin/product/show');
-});
-Route::get('/category', function () {
-    return view('admin/category/index');
-});
-Route::get('/category/create', function () {
-    return view('admin/category/create');
-});
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
+
+Route::group(['prefix'=>'/admin', 'middleware'=>['auth', 'admin']], function() {
+    Route::resource('product', 'ProductController');
+    Route::resource('category', 'CategoryController');
+    Route::resource('product_status', 'ProductController');
+    Route::resource('order_status', 'ProductController');
+
+    Route::get('/', ['as'=>'admin_dashboard', 'uses'=>'AdminController@dashboard']);
+});
+
+Route::group(['prefix'=>'/admin', 'middleware'=>['auth', 'operator']], function() {
+
+});
+
+Route::group(['prefix'=>'/admin', 'middleware'=>['auth', 'user']], function() {
+
+});
